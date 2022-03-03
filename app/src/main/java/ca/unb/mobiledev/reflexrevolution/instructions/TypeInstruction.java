c110fdb14259844d3d25a272184da95ebad69218
Carter Moore <cmoore12@unb.ca>
2022-03-03 17:11:40 -0400

    Added JSON word list
    Word list is stored in a JSON file, then read into an arraylist in memory when the game starts. We get a random word from this list whenever the instruction is triggered.

--- app/src/main/java/ca/unb/mobiledev/reflexrevolution/instructions/TypeInstruction.java
+++ app/src/main/java/ca/unb/mobiledev/reflexrevolution/instructions/TypeInstruction.java
@@ -1,53 +1,70 @@
 package ca.unb.mobiledev.reflexrevolution.instructions;
 
 import android.content.Context;
-import android.os.SystemClock;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.JsonReader;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Random;
-
-import ca.unb.mobiledev.reflexrevolution.detectors.TouchDetector;
 
 public class TypeInstruction extends Instruction{
     private Random rand;
     private String word;
-    private String[] wordList;
+    private EditText field;
+    private ArrayList<String> wordList;
     private InputMethodManager keyboardDisplayManager;
 
     public TypeInstruction(ViewGroup layout, Callback callback) {
         super(layout, callback);
         rand = new Random();
-        wordList = new String[]{"word"};
         keyboardDisplayManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        setUpWordList();
+    }
+
+    private void setUpWordList(){
+        try {
+            wordList = new ArrayList<>();
+            InputStream stream = layout.getContext().getAssets().open("TypeWords.json");
+            JsonReader reader = new JsonReader(new InputStreamReader(stream, "UTF-8"));
+            reader.beginArray();
+            while(reader.hasNext()){
+                wordList.add(reader.nextString());
+            }
+            reader.endArray();
+            reader.close();
+        } catch (IOException e){
+            Log.e("tag", "fail");
+        }
     }
 
     @Override
     public void init() {
         super.init();
         // Get random word to type
-        word = wordList[rand.nextInt(wordList.length)];
+        word = wordList.get(rand.nextInt(wordList.size()));
     }
 
     @Override
     public void display() {
         addTextView(word, SMALL_TEXT_SIZE);
         addTextView("TYPE", DEFAULT_TEXT_SIZE);
-        EditText textField = new EditText(context);
+        field = new EditText(context);
 
         //Listener that checks for text being updated
-        textField.addTextChangedListener(new TextWatcher() {
+        field.addTextChangedListener(new TextWatcher() {
             //If text equals current word, trigger success
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(s.toString().equals(word)){
+                if(s.toString().toUpperCase().equals(word)){
                     //Hide keyboard
-                    Log.d("tag", "Here");
                     keyboardDisplayManager.hideSoftInputFromWindow(layout.getWindowToken(), 0);
                     success();
                 }
@@ -62,23 +79,27 @@ public class TypeInstruction extends Instruction{
                 //Auto-generated
             }
         });
-        addView(textField);
+        addView(field);
+        enable();
+    }
 
-        //Select field, and force the keyboard to show
-        textField.requestFocus();
+    //Select field and force the keyboard to show
+    @Override
+    public void enable() {
+        field.requestFocus();
         keyboardDisplayManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
     }
 
+    //Close keyboard
     @Override
-    public void enable() {}
-
-    @Override
-    public void disable() {}
+    public void disable() {
+        keyboardDisplayManager.hideSoftInputFromWindow(layout.getWindowToken(), 0);
+    }
 
     @Override
     public void timerFinished() {
         //Hide keyboard
-        keyboardDisplayManager.hideSoftInputFromWindow(layout.getWindowToken(), 0);
+        disable();
         fail();
     }
 }
