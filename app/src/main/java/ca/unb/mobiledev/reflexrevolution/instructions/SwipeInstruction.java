cdc12f83c98fe73728ced08cdf6596e9a18f4f10
hsavoie <43451030+hsavoie@users.noreply.github.com>
2022-03-19 16:08:10 -0300

    Flick and Opposite Swipes Implemented (#12)
    * Flick and Opposite Swipes Implemented
    
    Added Flick (users fling in all directions)
    Added Swipe in opposite direction:
          - This is just implemented by having the displayed text being randomized between two options (ex: "RIGHT" and "OPPOSITE OF LEFT")
    
    * Updated Reverse Swipe
    
    - public enum Type {FLING, SWIPE} didnt get pushed last time PR so ensured it did this time
    - improved the way the titles are being stored
    - all other minor comments fixed
    
    * Updated Reverse Swipe
    
    Forgot to commit these changes
    
    * Renamed Fling enum to Flick
    
    Co-authored-by: Carter Moore <carterjmoore02@gmail.com>

--- app/src/main/java/ca/unb/mobiledev/reflexrevolution/instructions/SwipeInstruction.java
+++ app/src/main/java/ca/unb/mobiledev/reflexrevolution/instructions/SwipeInstruction.java
@@ -3,6 +3,9 @@ package ca.unb.mobiledev.reflexrevolution.instructions;
 
 import android.view.ViewGroup;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Random;
 
 import ca.unb.mobiledev.reflexrevolution.detectors.TouchDetector;
@@ -12,6 +15,9 @@ public class SwipeInstruction extends Instruction {
     private final TouchDetector touchDetector;
     private TouchDetector.SwipeAction currentAction;
     private Random rand;
+    private int index;
+    private final Map<TouchDetector.SwipeAction, String[]> textLabels = new HashMap<>();
+    private String displayTxt;
 
     public SwipeInstruction(ViewGroup layout, Callback callback, TouchDetector touchDetector) {
         super(layout, callback);
@@ -21,11 +27,20 @@ public class SwipeInstruction extends Instruction {
 
     private void setup() {
         rand = new Random();
+        textLabels.put(TouchDetector.SwipeAction.SWIPE_RIGHT, new String[] {"RIGHT", "OPPOSITE OF LEFT"});
+        textLabels.put(TouchDetector.SwipeAction.SWIPE_LEFT, new String[] {"LEFT", "OPPOSITE OF RIGHT"});
+        textLabels.put(TouchDetector.SwipeAction.SWIPE_UP, new String[] {"UP", "OPPOSITE OF DOWN"});
+        textLabels.put(TouchDetector.SwipeAction.SWIPE_DOWN, new String[] {"DOWN", "OPPOSITE OF UP"});
+
         touchDetector.addListener(new TouchDetector.ActionListener() {
             @Override
             public void onSwipe(TouchDetector.SwipeAction action) {
-                if (currentAction == action) success();
-                else fail(); // Wrong direction
+                if (currentAction.getType() == action.getType()) {
+                    // Same type and same instruction
+                    if (currentAction == action) success();
+                        // Same type but different instruction (wrong direction)
+                    else fail();
+                }
             }
             @Override
             public void onTap(TouchDetector.TapAction action) {}
@@ -39,16 +54,20 @@ public class SwipeInstruction extends Instruction {
         TouchDetector.SwipeAction[] actions = TouchDetector.SwipeAction.values();
         currentAction = actions[rand.nextInt(actions.length)];
     }
-
     @Override
     public void display() {
-        switch(currentAction) {
-            case SWIPE_RIGHT: addTextView("RIGHT", SMALL_TEXT_SIZE); break;
-            case SWIPE_LEFT: addTextView("LEFT", SMALL_TEXT_SIZE); break;
-            case SWIPE_UP: addTextView("UP", SMALL_TEXT_SIZE); break;
-            case SWIPE_DOWN: addTextView("DOWN", SMALL_TEXT_SIZE); break;
+        if (currentAction.getType() == TouchDetector.Type.SWIPE) {
+            // Add small label for swipe instructions
+            int index = rand.nextInt(2);
+            String displayTxt = Objects.requireNonNull(textLabels.get(currentAction))[index];
+            addTextView(displayTxt, SMALL_TEXT_SIZE);
+            // Main label
+            addTextView("SWIPE", DEFAULT_TEXT_SIZE);
         }
-        addTextView("SWIPE", DEFAULT_TEXT_SIZE);
+        else {
+            // Main label
+            addTextView("FLICK", DEFAULT_TEXT_SIZE);
+        }
     }
 
     @Override
