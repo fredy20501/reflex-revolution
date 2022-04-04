f98df088b1e709eda7f41f7d10103638e23a9f4a
Frederic Verret <43451024+fredy20501@users.noreply.github.com>
2022-04-04 16:09:05 -0300

    Create How to Play page

---
+++ app/src/main/java/ca/unb/mobiledev/reflexrevolution/activities/HowToPlayActivity.java
@@ -0,0 +1,16 @@
+package ca.unb.mobiledev.reflexrevolution.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.unb.mobiledev.reflexrevolution.R;
+
+public class HowToPlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.how_to_play);
+    }
+}
--- app/src/main/java/ca/unb/mobiledev/reflexrevolution/activities/MainActivity.java
+++ app/src/main/java/ca/unb/mobiledev/reflexrevolution/activities/MainActivity.java
@@ -30,14 +30,14 @@ public class MainActivity extends BackgroundMusicActivity {
             startActivity(intent);
         });
 
-//        Button howToPlayButton = findViewById(R.id.howToPlayButton);
-//        howToPlayButton.setOnClickListener(v -> {
-//            // Prevent double-clicking
-//            if (SystemClock.elapsedRealtime() - lastClickTime < 500) return;
-//            lastClickTime = SystemClock.elapsedRealtime();
-//            Intent intent = new Intent(MainActivity.this, HowToPlayActivity.class);
-//            startActivity(intent);
-//        });
+        Button howToPlayButton = findViewById(R.id.howToPlayButton);
+        howToPlayButton.setOnClickListener(v -> {
+            // Prevent double-clicking
+            if (SystemClock.elapsedRealtime() - lastClickTime < 500) return;
+            lastClickTime = SystemClock.elapsedRealtime();
+            Intent intent = new Intent(MainActivity.this, HowToPlayActivity.class);
+            startActivity(intent);
+        });
 
         Button settingsButton = findViewById(R.id.settingsButton);
         settingsButton.setOnClickListener(v -> {
